c003a1c2a721e40056c94e983e9c55fffdc3552f
Brad Ringel <brad.ringel@gmail.com>
2016-12-04 17:33:04 -0500

    initial layout of demo app

---
+++ no-animations/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { ShiplistComponent } from './shiplist/shiplist.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ShiplistComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ no-animations/src/app/shiplist/shiplist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'br-shiplist',
+  templateUrl: './shiplist.component.html',
+  styleUrls: ['./shiplist.component.scss']
+})
+export class ShiplistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
