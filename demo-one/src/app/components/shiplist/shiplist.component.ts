ec3de8986419a9c9c3b9435a0f662dba1b01fdf1
Brad Ringel <brad.ringel@gmail.com>
2016-12-07 22:52:08 -0500

    add fly in and out animations

--- demo-one/src/app/components/shiplist/shiplist.component.ts
+++ demo-one/src/app/components/shiplist/shiplist.component.ts
@@ -1,10 +1,19 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, trigger, state, style, transition, animate } from '@angular/core';
 import { Ship } from '../../model/ship.model';
 
 @Component({
   selector: 'br-shiplist',
   templateUrl: './shiplist.component.html',
-  styleUrls: ['./shiplist.component.scss']
+  styleUrls: ['./shiplist.component.scss'],
+  animations: [
+      trigger('flyIn', [
+        transition('void => *', [
+          style({transform: 'translateX(-100%)'}),
+          animate('500ms')
+        ]),
+        transition('* => void', animate('500ms', style({transform: 'translateX(-100%)'})))
+      ])
+  ]
 })
 export class ShiplistComponent implements OnInit {
 
