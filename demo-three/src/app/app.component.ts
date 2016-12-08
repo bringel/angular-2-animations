2e01d507937279d868542bc14abf7f6c783c79d8
Brad Ringel <brad.ringel@gmail.com>
2016-12-07 23:53:48 -0500

    Wiggle animations for the buttons. (pretty cool stuff)

--- demo-three/src/app/app.component.ts
+++ demo-three/src/app/app.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, trigger, state, style, animate, transition, keyframes } from '@angular/core';
 import { Ship } from './model/ship.model';
 import { ships as shipData} from './data/spaceships';
 
 @Component({
   selector: 'br-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('hoverTrigger', [
+      state('hovered', style({})),
+      transition('* => hovered', animate('250ms', keyframes([
+        style({transform: 'rotate(15deg)', offset: 0.25}),
+        style({transform: 'rotate(-15deg)', offset: 0.75}),
+        style({transform: 'rotate(0)', offset: 1})
+      ])))
+    ])
+  ]
 })
 export class AppComponent {
 
   private ships: Ship[];
   private selectedShip: Ship;
+  private addButtonHoverState: string;
+  private removeButtonHoverState: string;
 
   constructor() {
     this.ships = [];
@@ -28,5 +40,21 @@ export class AppComponent {
 
   shipSelected(index: number) {
     this.selectedShip = this.ships[index];
+  }
+
+  onAddButtonHover() {
+    this.addButtonHoverState = 'hovered';
+  }
+
+  onAddButtonUnHovered() {
+    this.addButtonHoverState = '';
+  }
+
+  onRemoveButtonHover() {
+    this.removeButtonHoverState = 'hovered';
+  }
+
+  onRemoveButtonUnHovered() {
+    this.removeButtonHoverState = '';
   }
 }
