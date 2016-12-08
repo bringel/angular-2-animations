f6a91e9d2850d184284a7b96004ea1c51f5291c4
Brad Ringel <brad.ringel@gmail.com>
2016-12-08 13:45:10 -0500

    add demo-four

---
+++ demo-four/src/app/data/spaceships.ts
@@ -0,0 +1,27 @@
+export const ships = [
+  {
+    name: 'Millenium Falcon',
+    description: ["She may not look like much, but she's got it where it counts kid.", "Once made the Kessel run in 12 parsecs"],
+    moreInfo: ['Franchise: Star Wars','Pilots: Han Solo, Chewbaca', 'Captain: Han Solo']
+  },
+  {
+    name: 'NCC-1701 Enterprise',
+    description: ['Constitution class starship sent on a 5 year mission to explore strange new worlds,', 'to seek out new life and new civilizations, to boldly go where no man has gone before.'],
+    moreInfo: ['Franchise: Star Trek', 'Pilot: Hikaru Sulu', 'Captain: James Kirk']
+  },
+  {
+    name: 'Serenity',
+    description: ['A shiny mid-bulk transport Firefly-class ship. The ship has no weapons, but the engine glows'],
+    moreInfo: ['Franchise: Serenity', 'Pilot: Hoban Washburne', 'Captain: Malcolm Reynolds']
+  },
+  {
+    name: 'Battlestar Galactica',
+    description: ['One of the first 12 Battlestars constructed by the Twelve Colonies', 'representing the planet Caprica. '],
+    moreInfo: ['Franchise: Battlestar Galactica', 'Captain: William Adama']
+  },
+  {
+    name: 'T.A.R.D.I.S',
+    description: ['Time And Relative Dimension In Space. Capable of travelling through time', 'typically reconfigures the rooms inside on a whim. Also flies through space sometimes'],
+    moreInfo: ['Franchise: Doctor Who', 'Pilots: The Doctor, River Song']
+  }
+];
