c6efb159be76a46d7fba7ae39f14551b4c3b7241
Brad Ringel <brad.ringel@gmail.com>
2016-12-07 23:12:34 -0500

    add demo-two

---
+++ demo-two/src/app/components/ship-detail/ship-detail.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'br-ship-detail',
+  templateUrl: './ship-detail.component.html',
+  styleUrls: ['./ship-detail.component.scss']
+})
+export class ShipDetailComponent implements OnInit {
+
+  @Input() detailText: string;
+  @Input() moreDetailText: string;
+
+  private expanded: boolean;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.expanded = false;
+  }
+
+  onExpandedClicked() {
+    this.expanded = !this.expanded;
+  }
+}
